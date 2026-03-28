16c8c200ace9624b9760dfd065a6c9861867205a
snowsugar <daisy831015@gmail.com>
2026-03-28 10:36:49 +0800

    Centralize form validation to fix form errors across pages

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 import './assets/scss/all.scss';
 import 'bootstrap'; 
 import './GSAP';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.prototype.slice.call(forms).forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+});
